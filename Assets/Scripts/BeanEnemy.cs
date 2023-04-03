8d86e48ca6697776106fd91043e6bfb5e93e35a8
E-LAMB <113103562+E-LAMB@users.noreply.github.com>
2023-04-04 00:17:33 +0100

    Linking Scenes Togther

--- Assets/Scripts/BeanEnemy.cs
+++ Assets/Scripts/BeanEnemy.cs
@@ -6,6 +6,7 @@ public class BeanEnemy : MonoBehaviour
 {
 
     public Transform self;
+    public Transform player;
 
     public Rigidbody2D my_body;
 
@@ -19,13 +20,18 @@ public class BeanEnemy : MonoBehaviour
     public LayerMask sight_layer;
 
     public Vector3 player_direction;
+    public Vector3 movement_direction;
     
     public float wakeup_range;
+
+    Vector3 zero;
+
+    public float speed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        self.localRotation = Quaternion.Euler(0f,0f,Random.Range(0f,360f));
     }
 
     // Update is called once per frame
@@ -46,17 +52,22 @@ public class BeanEnemy : MonoBehaviour
 
         } else
         {
-            in_sight = Physics2D.OverlapCircle(transform.position, 1f, sight_layer)
+            in_sight = Physics2D.OverlapCircle(transform.position, 1f, sight_layer);
 
             if (in_sight)
             {
                 anim_moving = false;
-
+                my_body.velocity = zero;
             } else
             {
                 anim_moving = true;
 
-                
+                player_direction = player.position - self.position;
+                player_direction.z = 0f;
+
+                movement_direction = Vector3.Normalize(player_direction);
+                movement_direction = movement_direction * speed;
+                my_body.AddForce(movement_direction);
             }
         }
 
