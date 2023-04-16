1280638281679293c496637e5d4298d46bf98960
E-LAMB <113103562+E-LAMB@users.noreply.github.com>
2023-04-16 14:09:34 +0100

    Playtester build has been sent out

--- Assets/Scripts/DontDestroyOnLoadMusic.cs
+++ Assets/Scripts/DontDestroyOnLoadMusic.cs
@@ -24,6 +24,7 @@ public class DontDestroyOnLoadMusic : MonoBehaviour
             if (new_track == 2) {my_source.clip = tracks[new_track];}
             if (new_track == 3) {my_source.clip = tracks[new_track];}
             if (new_track == 4) {my_source.clip = tracks[new_track];}
+            if (new_track == 5) {my_source.clip = tracks[new_track];} 
  
             my_source.Play();
                      
