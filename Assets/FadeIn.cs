47e1cf8f32ec0ed303d4bf24660096d40ebcbb43
E-LAMB <113103562+E-LAMB@users.noreply.github.com>
2023-07-26 18:53:33 +0100

    Added Fade on LOAD. Also began work on Dormis Hut redecorating,

---
+++ Assets/FadeIn.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeIn : MonoBehaviour
+{
+
+    public GameObject image_object;
+    public RawImage my_image;
+    public float time;
+
+    void Start()
+    {
+        time = 1f;
+        my_image.color = new Vector4(my_image.color.r, my_image.color.g, my_image.color.b, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        my_image.color = new Vector4(my_image.color.r, my_image.color.g, my_image.color.b, time);
+        if (-0.1f > time)
+        {
+            image_object.SetActive(false);
+        }
+    }
+}
